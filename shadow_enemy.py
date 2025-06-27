eea0f6eccefd5abf2b7062a53a5ac96f58b6648e
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 07:41:03 +0800

    update shadow enemy

--- shadow_enemy.py
+++ shadow_enemy.py
@@ -4,7 +4,12 @@ from settings import ASSET_PATH
 
 class ShadowEnemy(Enemy):
     def __init__(self):
-        self.image = pygame.image.load(ASSET_PATH + "shadow.png").convert_alpha()
+        try:
+            self.image = pygame.image.load(ASSET_PATH + "shadow.png").convert_alpha()
+        except pygame.error:
+            print("Warning: Missing shadow.png! Using placeholder.")
+            self.image = pygame.Surface((50, 50))
+            self.image.fill((0, 0, 0))
         super().__init__(self.image, -100, 500)
         
     def update(self, player):
@@ -12,4 +17,3 @@ class ShadowEnemy(Enemy):
             self.rect.x += 1
         elif self.rect.x > player.rect.x:
             self.rect.x -= 1
-            
