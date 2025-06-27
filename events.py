2ada4982a9d6a5b21ce1daf1c812a7f4bd1834c7
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 06:09:45 +0800

    set event conditions

--- events.py
+++ events.py
@@ -8,3 +8,19 @@ jumpscare_img = pygame.image.load(ASSET_PATH + 'jumpscare.png')
 current_event = None
 enemy = None
 
+def maybe_trigger_event():
+    global current_event, enemy
+    roll = random.randint(1, 10)
+    if roll == 1:
+        play_whisper()
+        current_event = "whisper"
+    elif roll == 2:
+        enemy = ShadowEnemy()
+        current_event = "enemy"
+    elif roll == 3:
+        play_scream()
+        current_event = "scream"
+    else:
+        current_event = None
+        enemy = None
+        
