ff9c297176751991e107447ff8125eac8be5d87a
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 05:42:43 +0800

    initialize whisper sound

---
+++ sounds.py
@@ -0,0 +1,15 @@
+import pygame
+from settings import ASSET_PATH
+
+whisper_sound = None
+scream_sound = None
+
+def init_sounds():
+    global whisper_sound, scream_sound
+    whisper_sound = pygame.mixer.Sound(ASSET_PATH + "whisper.wav")
+    scream_sound = pygame.mixer.Sound(ASSET_PATH + "scream.wav")
+    
+def play_whisper():
+    if whisper_sound:
+        whisper_sound.play()
+        
