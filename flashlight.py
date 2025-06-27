cb8df6cfb1b265a31792516a5580bcd7d97eafbf
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 06:48:02 +0800

    trial n error on flaslight file

--- flashlight.py
+++ flashlight.py
@@ -1,12 +1,11 @@
 import pygame
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def render_flashlight(screen, player_center):
-    overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-    overlay.fill((0, 0, 0))
+    overlay = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
+    overlay.fill((0, 0, 0, 200))
     
     radius = 100
-    pygame.draw.circle(overlay, (255, 255, 255), player_center, radius)
+    pygame.draw.circle(overlay, (0, 0, 0, 0), player_center, radius)
     
     screen.blit(overlay, (0, 0))
     
