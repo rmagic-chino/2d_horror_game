1dc9491260a237047f9236afa7ed76d28cc3433d
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 07:34:48 +0800

    update game object file

--- game_object.py
+++ game_object.py
@@ -2,6 +2,6 @@ class GameObject:
     def __init__(self, image, x, y):
         self.image = image
         self.rect = self.image.get_rect(topleft=(x, y))
-        
+
     def update(self):
         pass
