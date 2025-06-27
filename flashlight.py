afe1fdf35657092a165b43e681db263daf08d909
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 06:45:06 +0800

    update flashlight bugs

--- flashlight.py
+++ flashlight.py
@@ -1,12 +1,12 @@
 import pygame
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
-def render_flashlight(screen, player_rect):
+def render_flashlight(screen, player_center):
     overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     overlay.fill((0, 0, 0))
-    radius = 120
-    pygame.draw.circle(overlay, (255, 255, 255), player_rect.center, radius)
-    overlay.set_colorkey((255, 255, 255))
-    overlay.set.alpha(230)
+    
+    radius = 100
+    pygame.draw.circle(overlay, (255, 255, 255), player_center, radius)
+    
     screen.blit(overlay, (0, 0))
     
