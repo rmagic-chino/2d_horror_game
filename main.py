56d1abdfbdedd8f928b581ec9788b7a9c76f3b81
Carlos De Guzman <raremagic01@gmail.com>
2025-06-28 06:16:34 +0800

    import all from files

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+from settings import *
+from player import Player
+from room_manager import RoomManager
+from flashlight import render_flashlight
+from sounds import init_sounds
+from events import handle_events
+from save_manager import save_game, load_game
+
