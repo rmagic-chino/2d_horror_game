bd08091d2fb5c77d35fc5e03811554a6589c8c8f
rmagic-chino <raremagic01@gmail.com>
2025-06-28 11:34:44 +0800

    re-edit shadow enemy function

--- shadow_enemy.py
+++ shadow_enemy.py
@@ -5,16 +5,20 @@ from settings import ASSET_PATH
 class ShadowEnemy(Enemy):
     def __init__(self):
         try:
-            self.image = pygame.image.load(ASSET_PATH + "shadow.png").convert_alpha()
+            image = pygame.image.load(ASSET_PATH + "shadow.png").convert_alpha()
         except pygame.error:
-            print("Warning: Missing shadow.png, using placeholder.")
-            self.image = pygame.Surface((50, 50))
-            self.image.fill((0, 0, 0))
-        super().__init__(self.image, -100, 500)
+            image = pygame.Surface((40, 60))
+            image.fill((0, 0, 0))
+        super().__init__(image, -100, 300)
 
     def update(self, player):
-        speed = 3  # Faster ghost
+        speed = 2
         if self.rect.x < player.rect.x:
             self.rect.x += speed
         elif self.rect.x > player.rect.x:
             self.rect.x -= speed
+
+        if self.rect.y < player.rect.y:
+            self.rect.y += speed
+        elif self.rect.y > player.rect.y:
+            self.rect.y -= speed
