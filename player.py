d75021075c919d61e31ad76379683d38471e7cbe
rmagic-chino <raremagic01@gmail.com>
2025-06-28 12:40:45 +0800

    player update

--- player.py
+++ player.py
@@ -3,30 +3,16 @@ from settings import ASSET_PATH, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Player:
     def __init__(self):
-        self.image = pygame.image.load(ASSET_PATH + "player.png").convert_alpha()
-        self.rect = self.image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+        self.image = pygame.image.load(ASSET_PATH + "player.png")
+        self.rect = self.image.get_rect(midbottom=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 50))
         self.speed = 5
-        self.health = 3
 
     def handle_input(self, keys):
         if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
+            self.rect.x = max(0, self.rect.x - self.speed)
         if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
+            self.rect.x = min(SCREEN_WIDTH - self.rect.width, self.rect.x + self.speed)
         if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
+            self.rect.y = max(0, self.rect.y - self.speed)
         if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed
-
-        # Stay inside the screen
-        self.rect.clamp_ip(pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    def reset_position(self, exit_direction):
-        if exit_direction == 'left':
-            self.rect.right = SCREEN_WIDTH - 10
-        elif exit_direction == 'right':
-            self.rect.left = 10
-        elif exit_direction == 'up':
-            self.rect.bottom = SCREEN_HEIGHT - 10
-        elif exit_direction == 'down':
-            self.rect.top = 10
+            self.rect.y = min(SCREEN_HEIGHT - self.rect.height, self.rect.y + self.speed)
