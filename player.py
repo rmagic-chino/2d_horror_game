d44519030e2df7b85b46067c28685a32fe618eee
rmagic-chino <raremagic01@gmail.com>
2025-06-28 10:01:14 +0800

    add up and down movements

--- player.py
+++ player.py
@@ -1,17 +1,28 @@
-from typing import Any
 import pygame
-from settings import ASSET_PATH, SCREEN_WIDTH
-from game_object import GameObject
+from settings import ASSET_PATH
 
-class Player(GameObject):
+class Player:
     def __init__(self):
-        image = pygame.image.load(ASSET_PATH + "player.png")
-        super().__init__(image, 400, 550)
+        self.image = pygame.image.load(ASSET_PATH + "player.png")
+        self.rect = self.image.get_rect(midbottom=(400, 550))
         self.speed = 5
-        
-    def handle_input(self, keys: Any):
+
+    def handle_input(self, keys):
         if keys[pygame.K_LEFT]:
-            self.rect.x = max(0, self.rect.x - self.speed)
+            self.rect.x -= self.speed
         if keys[pygame.K_RIGHT]:
-            self.rect.x = min(SCREEN_WIDTH - self.rect.width, self.rect.x + self.speed)
-            
+            self.rect.x += self.speed
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed
+
+        # Keep the player on screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > 800:
+            self.rect.right = 800
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > 600:
+            self.rect.bottom = 600
