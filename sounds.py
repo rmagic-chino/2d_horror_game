b60deee23f6fb3af301c313b03ea5f5cb3b79be8
rmagic-chino <raremagic01@gmail.com>
2025-06-28 12:27:37 +0800

    re-edit sounds

--- sounds.py
+++ sounds.py
@@ -3,15 +3,14 @@ from settings import ASSET_PATH
 
 whisper_sound = None
 scream_sound = None
+jumpscare_scream = None  # NEW
 
 def init_sounds():
-    global whisper_sound, scream_sound
+    global whisper_sound, scream_sound, jumpscare_scream
     pygame.mixer.init()
     whisper_sound = pygame.mixer.Sound(ASSET_PATH + "whisper.wav")
     scream_sound = pygame.mixer.Sound(ASSET_PATH + "scream.wav")
-
-    whisper_sound.set_volume(1.0)
-    scream_sound.set_volume(1.0)
+    jumpscare_scream = pygame.mixer.Sound(ASSET_PATH + "jumpscare_scream.wav")  # NEW
 
 def play_whisper():
     if whisper_sound:
@@ -20,3 +19,7 @@ def play_whisper():
 def play_scream():
     if scream_sound:
         scream_sound.play()
+
+def play_jumpscare_scream():
+    if jumpscare_scream:
+        jumpscare_scream.play()
