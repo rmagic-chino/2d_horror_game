30194144eb51b224530c69a6aa4e38ecdb280bc5
rmagic-chino <raremagic01@gmail.com>
2025-06-28 11:35:27 +0800

    re-edit room function

--- room_manager.py
+++ room_manager.py
@@ -1,11 +1,11 @@
 import pygame
 import random
-from settings import ASSET_PATH, SCREEN_WIDTH, SCREEN_HEIGHT
+from settings import ASSET_PATH, SCREEN_WIDTH, SCREEN_HEIGHT, ROOM_LIMIT
 from events import maybe_trigger_event
 
 class RoomManager:
     def __init__(self):
-        self.room_image = pygame.image.load(ASSET_PATH + "room.png")
+        self.room_image = pygame.image.load(ASSET_PATH + "room.png").convert()
         self.rooms_passed = 0
         self.exit_side = random.choice(['left', 'right', 'up', 'down'])
 
@@ -23,17 +23,13 @@ class RoomManager:
 
         if room_changed:
             self.rooms_passed += 1
-            if self.rooms_passed < 20:
+            if self.rooms_passed < ROOM_LIMIT:
                 maybe_trigger_event()
                 self.exit_side = random.choice(['left', 'right', 'up', 'down'])
                 player.reset_position(self.exit_side)
             else:
-                self.end_game()
-
-    def end_game(self):
-        pygame.quit()
-        print("You survived all 20 rooms!")
-        exit()
+                return "end"
+        return None
 
     def get_current_room(self):
         return self.room_image
