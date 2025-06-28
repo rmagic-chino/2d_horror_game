d9c30be06905530fa5a95dff39575adf0d743800
rmagic-chino <raremagic01@gmail.com>
2025-06-28 11:30:52 +0800

    edit player settings

--- player.py
+++ player.py
@@ -1,11 +1,14 @@
+# player.py
+
 import pygame
 from settings import ASSET_PATH, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Player:
     def __init__(self):
-        self.image = pygame.image.load(ASSET_PATH + "player.png")
-        self.rect = self.image.get_rect(midbottom=(400, 550))
+        self.image = pygame.image.load(ASSET_PATH + "player.png").convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 50))
         self.speed = 5
+        self.health = 3
 
     def handle_input(self, keys):
         if keys[pygame.K_LEFT]:
@@ -17,7 +20,6 @@ class Player:
         if keys[pygame.K_DOWN]:
             self.rect.y += self.speed
 
-        # Keep on screen
         self.rect.clamp_ip(pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
 
     def reset_position(self, exit_direction):
