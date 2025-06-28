7e851a844748b1255206af002cd1cb36f56b6922
rmagic-chino <raremagic01@gmail.com>
2025-06-28 11:47:23 +0800

    update shadow

--- shadow_enemy.py
+++ shadow_enemy.py
@@ -1,24 +1,33 @@
 import pygame
 from enemy import Enemy
 from settings import ASSET_PATH
+from sounds import play_scream
 
 class ShadowEnemy(Enemy):
     def __init__(self):
-        try:
-            image = pygame.image.load(ASSET_PATH + "shadow.png").convert_alpha()
-        except pygame.error:
-            image = pygame.Surface((40, 60))
-            image.fill((0, 0, 0))
-        super().__init__(image, -100, 300)
+        image = pygame.image.load(ASSET_PATH + "shadow.png").convert_alpha()
+        super().__init__(image, -100, -100)
+        self.active = False
 
     def update(self, player):
+        if not self.active:
+            self.rect.topleft = (player.rect.x - 200, player.rect.y)
+            self.active = True
+
         speed = 2
         if self.rect.x < player.rect.x:
             self.rect.x += speed
-        elif self.rect.x > player.rect.x:
+        if self.rect.x > player.rect.x:
             self.rect.x -= speed
-
         if self.rect.y < player.rect.y:
             self.rect.y += speed
-        elif self.rect.y > player.rect.y:
+        if self.rect.y > player.rect.y:
             self.rect.y -= speed
+
+        if self.distance_to(player) < 10:
+            play_scream()
+
+    def distance_to(self, player):
+        dx = self.rect.centerx - player.rect.centerx
+        dy = self.rect.centery - player.rect.centery
+        return (dx**2 + dy**2) ** 0.5
